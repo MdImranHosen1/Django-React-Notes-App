b943da32798f4ec804b84e47c491fbd6ee97224a
Md Imran Hosen <62979868+MdImranHosen1@users.noreply.github.com>
2023-12-20 01:49:22 +0600

    "Added update note feture"

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -22,17 +22,28 @@ export const NotePage = () => {
         getNote();
     }, [id]);
 
+    let updataNote= async()=>{
+
+        fetch(`/api/notes/${id}/update/`, {
+            method: "PUT",
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(note)
+        })
+    }
+
     return (
         <>
             <div className="note">
                 <div className="note-header">
                     <h3>
                         <Link to="/">
-                        <ArrowLeft/>
+                        <ArrowLeft onClick={updataNote}/>
                         </Link>
                     </h3>
                 </div>
-                <textarea defaultValue={note?.body}>
+                <textarea onChange={(e)=>{setNote({...note,'body':e.target.value})}} defaultValue={note?.body}>
                     
                 </textarea>
             </div>
--- mynotes/api/views.py
+++ mynotes/api/views.py
@@ -48,7 +48,7 @@ def getRoutes(request):
 @api_view(['GET'])
 def getNotes(request):
     
-    notes=Note.objects.all()
+    notes=Note.objects.all().order_by('-updated')
     serializer=NoteSerializer(notes, many=True)
     return Response(serializer.data)
 
@@ -58,4 +58,14 @@ def getNote(request,pk):
     
     notes=Note.objects.get(id=pk)
     serializer=NoteSerializer(notes, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateNote(request,pk):
+    data= request.data
+    notes=Note.objects.get(id=pk)
+    serializer=NoteSerializer(instance=notes,data=data)
+    if serializer.is_valid():
+        serializer.save()
+        
     return Response(serializer.data)
